adf58e2651777661ac1c114b1c9cdd6eba3263dd
Abolfazl <73878628+AboulfazlRezaee@users.noreply.github.com>
2026-02-09 14:58:13 +0330

    Update price

--- simple_warehouse_sync/__manifest__.py
+++ simple_warehouse_sync/__manifest__.py
@@ -52,7 +52,7 @@
     "author": "Abolfazl Rezaei",
     "category": "Inventory",
     "license": "OPL-1",
-    "price": 69.0,
+    "price": 85.0,
     "currency": "EUR",
     "depends": ["web", "stock"],
     "data": [
